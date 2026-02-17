051e209c86b2a8b2f6450085fbddcd23c597d623
Шавкат <master1brave@gmail.com>
2026-02-17 08:46:44 +0300

    Update tasks table migration

--- database/migrations/2026_02_17_044047_create_tasks_table.php
+++ database/migrations/2026_02_17_044047_create_tasks_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
